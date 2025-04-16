1e2fb5cf7dedb8391a6ae536ec91724f93c2f421
saretta0789 <saravillano2004@gmail.com>
2025-04-16 21:09:16 +0200

    modifica

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,15 @@ import model.Utente;
 public class Main {
     public static void main(String[] args) {
 
+<<<<<<< Updated upstream
         Utente u1 = new Utente("mike@gmail.com", "mikemike");
 
+=======
+
+        Utente u1 = new Utente("Utente 1", "mike@gmail.com", "mikemike");
+>>>>>>> Stashed changes
         System.out.println(u1.getLogin());
         System.out.println(u1.getPassword());
+
     }
 }
