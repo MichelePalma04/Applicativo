b0e03a4cdf90405842294f86f6b0d58fe9b600cc
MichelePalma04 <michelepalma04@gmail.com>
2025-04-16 21:11:47 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,9 @@ import model.Utente;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-<<<<<<< Updated upstream
+        
         Utente u1 = new Utente("mike@gmail.com", "mikemike");
 
-=======
-
-        Utente u1 = new Utente("Utente 1", "mike@gmail.com", "mikemike");
->>>>>>> Stashed changes
         System.out.println(u1.getLogin());
         System.out.println(u1.getPassword());
 
