5307270f33cd575767546d5a7520a8e7e6338b9b
MichelePalma04 <michelepalma04@gmail.com>
2025-04-16 21:49:48 +0200

    modifica
    aggiunta exstends

--- src/main/java/model/Giudice.java
+++ src/main/java/model/Giudice.java
@@ -1,7 +1,15 @@
 package model;
 
-public class Giudice {
-
+public class Giudice extends Utente {
+    /**
+     * Instantiates a new Utente.
+     *
+     * @param login    the login
+     * @param password the password
+     */
+    public Giudice(String login, String password) {
+        super(login, password);
+    }
     public void esaminaDocumento() {}
 
     public void pubblicaProblema() {}
--- src/main/java/model/Partecipante.java
+++ src/main/java/model/Partecipante.java
@@ -1,5 +1,14 @@
 package model;
 
-public class Partecipante {
+public class Partecipante extends Utente {
 
+    /**
+     * Instantiates a new Utente.
+     *
+     * @param login    the login
+     * @param password the password
+     */
+    public Partecipante(String login, String password) {
+        super(login, password);
+    }
 }
