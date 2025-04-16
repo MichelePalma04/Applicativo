31afd1bd691c1e4a25883167b1eaca577486571b
saretta0789 <saravillano2004@gmail.com>
2025-04-16 21:51:02 +0200

    aggiornamento extends giudice

--- src/main/java/model/Organizzatore.java
+++ src/main/java/model/Organizzatore.java
@@ -1,6 +1,16 @@
 package model;
 
-public class Organizzatore {
+public class Organizzatore extends Utente {
+
+    /**
+     * Instantiates a new Utente.
+     *
+     * @param login    the login
+     * @param password the password
+     */
+    public Organizzatore(String login, String password) {
+        super(login, password);
+    }
 
     public void invitoGiudici(){}
     public void aperturaRegistrazione(){}
