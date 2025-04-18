e39898e45e0233b3e1496b4a351d270f04a4c2ab
MichelePalma04 <michelepalma04@gmail.com>
2025-04-18 21:04:53 +0200

    update team

--- src/main/java/model/Team.java
+++ src/main/java/model/Team.java
@@ -5,16 +5,36 @@ import java.util.ArrayList;
 public class Team {
     private String nomeTeam;
     private ArrayList<Documento> documenti;
+    private ArrayList<Partecipante> partecipanti;
 
-    public Team (String nomeTeam) {
+    public Team (String nomeTeam, ArrayList<Partecipante> partecipanti) {
         this.nomeTeam = nomeTeam;
+        this.partecipanti = partecipanti;
         this.documenti = new ArrayList<>();
+    }
+
+    public Team (String nomeTeam, Partecipante p) {
+        this.nomeTeam = nomeTeam;
+        partecipanti = new ArrayList<>();
+        partecipanti.add(p);
+        this.documenti = new ArrayList<>();
+
+    }
+
+    public void addPartecipanti(Partecipante newP) {
+        if (!partecipanti.contains(newP)) {
+            partecipanti.add(newP);
+        }
     }
 
     public void addDocumento(Documento newDoc) {
         if (!this.documenti.contains(newDoc)) {
             this.documenti.add(newDoc);
         }
+    }
+
+    public ArrayList<Partecipante> getPartecipanti() {
+        return partecipanti;
     }
 
     public String getNomeTeam() {
