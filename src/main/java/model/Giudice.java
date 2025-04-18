0d1b9a666ed2d82b83d23e2ee0162dc76c2de9fe
saretta0789 <saravillano2004@gmail.com>
2025-04-18 21:20:41 +0200

    Update Giudice.java

--- src/main/java/model/Giudice.java
+++ src/main/java/model/Giudice.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 
 public class Giudice extends Utente {
     private ArrayList<Evento> eventi;
+    private ArrayList<Voto> voti;
 
-    public Giudice(String login, String password,ArrayList<Evento> eventi) {
+    public Giudice(String login, String password,ArrayList<Evento> eventi, ArrayList<Voto> voti) {
         super(login, password);
         this.eventi = eventi;
+        this.voti = voti;
     }
 
-    public Giudice(String login, String password, Evento e) {
+    public Giudice(String login, String password, Evento e, Voto v) {
         super(login, password);
         eventi = new ArrayList<>();
         eventi.add(e);
+        voti = new ArrayList<>();
+        voti.add(v);
     }
 
     public void addEventi(Evento newE){
@@ -22,8 +26,18 @@ public class Giudice extends Utente {
         }
     }
 
+    public void addVoti(Voto newV){
+        if(!this.voti.contains(newV)){
+            this.voti.add(newV);
+        }
+    }
+
    public ArrayList<Evento> getEventi() {
         return this.eventi;
+   }
+
+   public ArrayList<Voto> getVoti() {
+        return this.voti;
    }
 
     public void esaminaDocumento() {}
