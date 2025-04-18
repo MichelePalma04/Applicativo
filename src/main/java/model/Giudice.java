8fd964a026fd69bb007361d3f5abe0579cc4bb33
saretta0789 <saravillano2004@gmail.com>
2025-04-18 23:22:09 +0200

    Update Giudice.java

--- src/main/java/model/Giudice.java
+++ src/main/java/model/Giudice.java
@@ -5,19 +5,23 @@ import java.util.ArrayList;
 public class Giudice extends Utente {
     private ArrayList<Evento> eventi;
     private ArrayList<Voto> voti;
+    private ArrayList <Organizzatore> organizzatori;
 
-    public Giudice(String login, String password, ArrayList<Evento> eventi, ArrayList<Voto> voti) {
+    public Giudice(String login, String password, ArrayList<Evento> eventi, ArrayList<Voto> voti, ArrayList<Organizzatore> organizzatori) {
         super(login, password);
         this.eventi = eventi;
         this.voti = voti;
+        this.organizzatori = organizzatori;
     }
 
-    public Giudice(String login, String password, Evento e, Voto v) {
+    public Giudice(String login, String password, Evento e, Voto v, Organizzatore o) {
         super(login, password);
         eventi = new ArrayList<>();
         eventi.add(e);
         voti = new ArrayList<>();
         voti.add(v);
+        organizzatori = new ArrayList<>();
+        organizzatori.add(o);
     }
 
     public void addEventi(Evento newE){
@@ -32,18 +36,28 @@ public class Giudice extends Utente {
         }
     }
 
-   public ArrayList<Evento> getEventi() {
+    public void addOrganizzatore(Organizzatore newO){
+        if(!this.organizzatori.contains(newO)){
+            this.organizzatori.add(newO);
+        }
+    }
+
+    public ArrayList<Evento> getEventi() {
         return this.eventi;
-   }
-
-   public ArrayList<Voto> getVoti() {
+    }
+    public ArrayList<Voto> getVoti() {
         return this.voti;
-   }
+    }
 
-   @Override
-   public String toString(){
+    public ArrayList<Organizzatore> getOrganizzatori() {
+        return this.organizzatori;
+    }
+
+    @Override
+    public String toString(){
         return getLogin();
-   }
+    }
+
     public void esaminaDocumento() {}
 
     public void pubblicaProblema() {}
