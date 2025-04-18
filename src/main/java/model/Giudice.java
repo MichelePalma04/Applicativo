a0ad76694aa14fe906946bca77da5d2995dc49d6
saretta0789 <saravillano2004@gmail.com>
2025-04-18 20:53:21 +0200

    modifica associazioni

--- src/main/java/model/Giudice.java
+++ src/main/java/model/Giudice.java
@@ -1,15 +1,31 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Giudice extends Utente {
-    /**
-     * Instantiates a new Utente.
-     *
-     * @param login    the login
-     * @param password the password
-     */
-    public Giudice(String login, String password) {
+    private ArrayList<Evento> eventi;
+
+    public Giudice(String login, String password,ArrayList<Evento> eventi) {
         super(login, password);
+        this.eventi = eventi;
     }
+
+    public Giudice(String login, String password, Evento e) {
+        super(login, password);
+        eventi = new ArrayList<>();
+        eventi.add(e);
+    }
+
+    public void addEventi(Evento newE){
+        if(!this.eventi.contains(newE)){
+            this.eventi.add(newE);
+        }
+    }
+
+   public ArrayList<Evento> getEventi() {
+        return this.eventi;
+   }
+
     public void esaminaDocumento() {}
 
     public void pubblicaProblema() {}
--- src/main/java/model/Partecipante.java
+++ src/main/java/model/Partecipante.java
@@ -1,15 +1,30 @@
 package model;
+import java.util.ArrayList;
 
 public class Partecipante extends Utente {
     private Team team;
-    /**
-     * Instantiates a new Utente.
-     *
-     * @param login    the login
-     * @param password the password
-     */
-    public Partecipante(String login, String password, Team team) {
+    private ArrayList<Evento> eventi;
+
+    public Partecipante(String login, String password, Team team, ArrayList<Evento> eventi) {
         super(login, password);
         this.team = team;
+        this.eventi = eventi;
+    }
+
+    public Partecipante(String login, String password, Team team, Evento e) {
+        super(login, password);
+        this.team = team;
+        eventi = new ArrayList<>();
+        eventi.add(e);
+    }
+
+    public void addEventi (Evento newE){
+        if(!this.eventi.contains(newE)){
+            this.eventi.add(newE);
+        }
+    }
+
+    public ArrayList<Evento> getEventi(){
+        return this.eventi;
     }
 }
