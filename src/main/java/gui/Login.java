3edc81d6793f407b7ea2ebec0a3538133fc0d789
MichelePalma04 <michelepalma04@gmail.com>
2025-05-20 17:12:54 +0200

    aggiunta di login.form

---
+++ src/main/java/gui/Login.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.*;
+
+public class Login {
+    private JPanel Login;
+    private JLabel mail;
+    private JTextField mailField;
+    private JLabel password;
+    private JTextField passwordField;
+    private JButton accedi;
+    private JButton registrati;
+
+    
+
+
+}
